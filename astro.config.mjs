dc26308a0e42251ef378aae627140243e44e0883
Syméon Smith <symeon.smith@gmail.com>
2025-04-27 19:56:23 +0200

    configure site url and syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { transformerColorizedBrackets } from "@shikijs/colorized-brackets";
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+
+const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 export default defineConfig({
-	site: "https://astro-nano-demo.vercel.app",
+	site: SITE_URL,
+	markdown: {
+		shikiConfig: {
+			transformers: [transformerColorizedBrackets()],
+			themes: { light: "one-light", dark: "slack-dark" },
+		},
+	},
 	integrations: [mdx(), sitemap(), tailwind()],
 	trailingSlash: "always",
 });
