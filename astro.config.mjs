14751aeb2e96a8538d395664b49c8be84f0185f6
Syméon Smith <symeon.smith@gmail.com>
2025-05-02 14:04:29 +0200

    get first paragraph as post description

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import {
 } from "@shikijs/transformers";
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
+import remarkDescription from "./plugins/description";
 
 const { SITE_URL = "http://localhost:4321" } = loadEnv(
 	process.env.NODE_ENV,
@@ -30,6 +31,7 @@ export default defineConfig({
 			],
 			themes: { light: "one-light", dark: "slack-dark" },
 		},
+		remarkPlugins: [remarkDescription],
 	},
 	integrations: [
 		mdx(),
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -14,15 +14,19 @@ export async function GET(context: Context) {
 		(a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
 	);
 
+	const awaitedItems = await Promise.all(
+		items.map(async (item) => ({
+			title: item.data.title,
+			description: await getDescription(item),
+			pubDate: item.data.date,
+			link: `/${item.collection}/${item.slug}/`,
+		})),
+	);
+
 	return rss({
 		title: HOME.TITLE,
 		description: HOME.DESCRIPTION,
 		site: context.site,
-		items: items.map((item) => ({
-			title: item.data.title,
-			description: getDescription(item.body),
-			pubDate: item.data.date,
-			link: `/${item.collection}/${item.slug}/`,
-		})),
+		items: awaitedItems,
 	});
 }
