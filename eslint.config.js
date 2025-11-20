ff3db0d207883dec9b78e6667828d8c81a6b54c8
Syméon Smith <symeon.smith@gmail.com>
2025-11-20 18:58:15 +0100

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,40 @@
+import eslint from "@eslint/js";
+import astroParser from "astro-eslint-parser";
+import gitignore from "eslint-config-flat-gitignore";
+import astroPlugin from "eslint-plugin-astro";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
-import astroParser from "astro-eslint-parser";
-import astroPlugin from "eslint-plugin-astro";
-import eslint from "@eslint/js";
 import tsESLint from "typescript-eslint";
-import gitignore from "eslint-config-flat-gitignore";
 
-export default defineConfig(
-	tsESLint.config([
-		gitignore(),
-		globalIgnores([".vscode/", "**/public/"]),
-		eslint.configs.recommended,
-		tsESLint.configs.recommended,
-		...astroPlugin.configs.recommended,
-		{
-			languageOptions: {
-				globals: {
-					...globals.node,
-					...globals.browser,
-				},
-				sourceType: "module",
+export default defineConfig([
+	gitignore(),
+	globalIgnores([".vscode/", "**/public/"]),
+	eslint.configs.recommended,
+	tsESLint.configs.recommended,
+	...astroPlugin.configs.recommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.browser,
 			},
-			rules: {
-				semi: ["error", "always"],
-				"@typescript-eslint/triple-slash-reference": "off",
+			sourceType: "module",
+		},
+		rules: {
+			semi: ["error", "always"],
+			"@typescript-eslint/triple-slash-reference": "off",
+		},
+	},
+	{
+		files: ["**/*.astro"],
+		languageOptions: {
+			parser: astroParser,
+			sourceType: "script",
+
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+				extraFileExtensions: [".astro"],
 			},
 		},
-		{
-			files: ["**/*.astro"],
-			languageOptions: {
-				parser: astroParser,
-				sourceType: "script",
-
-				parserOptions: {
-					parser: "@typescript-eslint/parser",
-					extraFileExtensions: [".astro"],
-				},
-			},
-		},
-	]),
-);
+	},
+]);
